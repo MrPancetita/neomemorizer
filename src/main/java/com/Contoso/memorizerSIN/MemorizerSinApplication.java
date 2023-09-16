6ed9af78ccb12ec2fd9724501d412405d138ca4e
MrPancetita <contreras.engineer@outlook.com>
2023-09-16 16:53:16 +0200

    initial spring project

---
+++ src/main/java/com/Contoso/memorizerSIN/MemorizerSinApplication.java
@@ -0,0 +1,13 @@
+package com.Contoso.memorizerSIN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorizerSinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorizerSinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Contoso/memorizerSIN/MemorizerSinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Contoso.memorizerSIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizerSinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
