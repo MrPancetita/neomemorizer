b3e68a5dabd92a6bf3563afc9a9ae5a4f3a347be
MrPancetita <contreras.engineer@outlook.com>
2023-09-23 11:37:45 +0200

    renaming finished

--- src/main/java/com/contoso/neomemorizer/NeoMemorizerApplication.java
+++ src/main/java/com/contoso/neomemorizer/NeoMemorizerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MemorizerSinApplication {
+public class NeoMemorizerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MemorizerSinApplication.class, args);
+		SpringApplication.run(NeoMemorizerApplication.class, args);
 	}
 
 }
